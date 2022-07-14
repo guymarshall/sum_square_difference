1fc1fed9d754de0f289c50e88fbbc55868b653f6
Guy Marshall <guymarshall9@gmail.com>
2022-07-14 22:34:46 +0100

    Add explicit typing for vectors

--- src/math.rs
+++ src/math.rs
@@ -1,5 +1,5 @@
 pub fn number_to_vector(number: u128) -> Vec<u128> {
-    let mut numbers = Vec::new();
+    let mut numbers: Vec<u128> = Vec::new();
 
     for i in 1..number + 1 {
         numbers.push(i);
@@ -9,7 +9,7 @@ pub fn number_to_vector(number: u128) -> Vec<u128> {
 }
 
 pub fn square_vector_values(values: &mut Vec<u128>) -> Vec<u128> {
-    let mut squared_values = Vec::new();
+    let mut squared_values: Vec<u128> = Vec::new();
 
     for value in values.iter() {
         squared_values.push(value * value);
