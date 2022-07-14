1171f0f133a60c61df263beb9f79442229d73dec
Guy Marshall <guymarshall9@gmail.com>
2022-07-14 22:44:09 +0100

    Finish main function

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,11 @@ mod user_input;
 fn main() {
     let number: u128 = user_input::get_user_input("Number:");
     let numbers: Vec<u128> = math::number_to_vector(number);
+    let squared_values: Vec<u128> = math::square_vector_values(&numbers);
+    let sum_of_squared_values: u128 = sum::sum(&squared_values);
+    let sum_of_values: u128 = sum::sum(&numbers);
+    let squared_sum: u128 = sum_of_values.pow(2);
+    let difference: u128 = squared_sum - sum_of_squared_values;
+
+    println!("Difference between {} and {} is: {}", squared_sum, sum_of_squared_values, difference);
 }
-
-/*
-
-CONVERT TO RUST
-
-std::vector<long long> squaredValues = squareVectorValues(numbers);
-long long sumOfSquaredValues = sum(squaredValues);
-long long sumOfValues = sum(numbers);
-
-long long squaredSum = pow(sumOfValues, 2);
-long long difference = squaredSum - sumOfSquaredValues;
-
-std::cout << "Difference between " << squaredSum << " and " << sumOfSquaredValues << " is: " << difference << std::endl;
-return 0;*/
