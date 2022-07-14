4559cf943dbd6072959b9a4ceabaf9cd0d511bd5
Guy Marshall <guy@cslgroupservices.co.uk>
2022-07-14 12:34:54 +0100

    Add C++ code to convert into Rust code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/*
+
+CONVERT TO RUST
+#include <iostream>
+#include "Sum.hpp"
+#include <vector>
+#include "Math.hpp"
+#include <cmath>
+
+#define LOG(x) std::cout << x << std::endl
+
+int main(int argc, char const *argv[])
+{
+    std::cout <<"Number: ";
+    long long number;
+    std::cin >> number;
+
+    std::vector<long long> numbers = numberToVector(number);
+
+    std::vector<long long> squaredValues = squareVectorValues(numbers);
+    long long sumOfSquaredValues = sum(squaredValues);
+    long long sumOfValues = sum(numbers);
+
+    long long squaredSum = pow(sumOfValues, 2);
+    long long difference = squaredSum - sumOfSquaredValues;
+    
+    std::cout << "Difference between " << squaredSum << " and " << sumOfSquaredValues << " is: " << difference << std::endl;
+    return 0;
+} */
