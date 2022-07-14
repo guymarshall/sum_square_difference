fb9f0552fdab96e34028d0e63f98f599a397b3fb
Guy Marshall <guymarshall9@gmail.com>
2022-07-14 22:25:51 +0100

    Fix math.rs

--- src/math.rs
+++ src/math.rs
@@ -11,8 +11,8 @@ pub fn number_to_vector(number: u128) -> Vec<u128> {
 pub fn square_vector_values(values: &mut Vec<u128>) -> Vec<u128> {
     let mut squared_values = Vec::new();
 
-    for i in 1..values.len() {
-        squared_values.push(values[i] ** 2);
+    for value in values.iter() {
+        squared_values.push(value * value);
     }
 
     return squared_values;
