a14a4a086df5b99e0601c8b92de29df3b1606237
Guy Marshall <guymarshall9@gmail.com>
2022-07-14 22:35:48 +0100

    Add up to number_to_vector in main

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,20 @@ mod sum;
 mod user_input;
 
 fn main() {
-    println!("Hello, world!");
+    let number: u128 = user_input::get_user_input("Number:");
+    let numbers: Vec<u128> = math::number_to_vector(number);
 }
 
 /*
 
 CONVERT TO RUST
 
-int main(int argc, char const *argv[])
-{
-    std::cout <<"Number: ";
-    long long number;
-    std::cin >> number;
+std::vector<long long> squaredValues = squareVectorValues(numbers);
+long long sumOfSquaredValues = sum(squaredValues);
+long long sumOfValues = sum(numbers);
 
-    std::vector<long long> numbers = numberToVector(number);
+long long squaredSum = pow(sumOfValues, 2);
+long long difference = squaredSum - sumOfSquaredValues;
 
-    std::vector<long long> squaredValues = squareVectorValues(numbers);
-    long long sumOfSquaredValues = sum(squaredValues);
-    long long sumOfValues = sum(numbers);
-
-    long long squaredSum = pow(sumOfValues, 2);
-    long long difference = squaredSum - sumOfSquaredValues;
-    
-    std::cout << "Difference between " << squaredSum << " and " << sumOfSquaredValues << " is: " << difference << std::endl;
-    return 0;
-} */
+std::cout << "Difference between " << squaredSum << " and " << sumOfSquaredValues << " is: " << difference << std::endl;
+return 0;*/
